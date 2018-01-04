483c19a0f61546d26048ad36bc2622a992bfb464
Rens Hamburger <renshamburger@gmail.com>
2018-01-04 11:34:49 +0100

    Improve styling

--- templates/nope.php
+++ templates/nope.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes">
+
+    <meta name="description" content="Kevin betaalt altijd alles">
+
+    <!--    <link rel="icon" href="/images/favicon.png">-->
+
+    <!-- See https://goo.gl/qRE0vM -->
+    <!--    <meta name="theme-color" content="#000">-->
+
+    <!-- Tile icon for Windows 8 (144x144 + tile color) -->
+    <!--    <meta name="msapplication-TileImage" content="/images/manifest/icon-144x144.png">-->
+    <!--    <meta name="msapplication-TileColor" content="#000">-->
+    <!--    <meta name="msapplication-tap-highlight" content="no">-->
     <meta charset="UTF-8">
     <title>Natuurlijk</title>
     <link rel="stylesheet" href="/assets/css/main.css"/>
