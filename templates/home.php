5c4266b06c584c18e548eb67bff2e39e9682eb09
Rens Hamburger <renshamburger@gmail.com>
2018-01-04 17:59:06 +0100

    Try a fix for footer

--- templates/home.php
+++ templates/home.php
@@ -20,17 +20,20 @@
           integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
 </head>
 <body>
-<div class="home">
-    <h1 class="font-weight-bold home">Kevin betaalt altijd alles</h1>
-    <p class="home">Wegens een overschot aan geld is Kevin begonnen met alles voor iedereen betalen. Voor verdere
-        informatie kunt u
-        contact opnemen met Kevin, te bereiken op <a href="mailto:declaratie@kevinbetaaltalles.nl" target="#">declaratie@kevinbetaaltalles.nl</a>.
-    </p>
+<div class="container">
+    <div class="text-center">
+        <h1 class="font-weight-bold home">Kevin betaalt altijd alles</h1>
+        <p class="home">Wegens een overschot aan geld is Kevin begonnen met alles voor iedereen betalen. Voor verdere
+            informatie kunt u
+            contact opnemen met Kevin, te bereiken op <a href="mailto:declaratie@kevinbetaaltalles.nl" target="#">declaratie@kevinbetaaltalles.nl</a>.
+        </p>
+    </div>
 </div>
 
-
-</body>
-<footer class="footer align-bottom">
-    Er zijn al <?= $count ?> betallingen gemaakt door Kevin met een totale waarde van &euro; <?= $total ?>
+<footer class="footer">
+    <div class="container">
+        <span class="text-muted text-center">Er zijn al <?= $count ?> betallingen gemaakt door Kevin met een totale waarde van &euro; <?= $total ?></span>
+    </div>
 </footer>
+</body>
 </html>
